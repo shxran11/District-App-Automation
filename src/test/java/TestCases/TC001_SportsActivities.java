79562c5b3899e2887099d1dc5812ed2d82d41e9f
Sharanya <sharanyapodder18@gmail.com>
2025-11-21 23:39:20 +0530

    Implemented logging with log4j

--- src/test/java/TestCases/TC001_SportsActivities.java
+++ src/test/java/TestCases/TC001_SportsActivities.java
@@ -1,36 +1,54 @@
 package TestCases;
-
+ 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
+ 
 import TestBase.BaseClass;
 import pageObjects.ActivitiesFactory;
 import pageObjects.HomeFactory;
-
+ 
 public class TC001_SportsActivities extends BaseClass {
-
+ 
 		
 	    @Test(priority=1)
 	    public void displayCheapestActivity() {
-	        HomeFactory home = new HomeFactory(driver);
-	        ActivitiesFactory activities = new ActivitiesFactory(driver);
-
-	        home.clickActivities();
-	        activities.clickGamesZone();
-	        activities.clickDayFilter();
-	        activities.clickFiltersButton();
-	        activities.clickSortOption();
-	        activities.clickApplyFilters();
-	        
-
-	        String gameName = activities.getCheapestGame();
-	        String gameTime = activities.getCheapestGameTime();
-
-	        System.out.println("Cheapest Activity This Weekend: " + gameName);
-	        System.out.println("Date/Time: " + gameTime);
-	        Assert.assertTrue(gameName != null && !gameName.isEmpty(), "Game name should be displayed");
+	    	logger.info("*****Starting TC001_SportsActivities*****");
+	    	
+	    	try 
+	    	{
+		        HomeFactory home = new HomeFactory(driver);
+		        ActivitiesFactory activities = new ActivitiesFactory(driver);
+	 
+		        home.clickActivities();
+		        logger.info("Clicked on Activities button");
+		        activities.clickGamesZone();
+		        logger.info("Clicked on Games Zone");
+		        activities.clickDayFilter();
+		        logger.info("Clicked on day filter");
+		        activities.clickFiltersButton();
+		        logger.info("Filter is selected");
+		        activities.clickSortOption();
+		        activities.clickApplyFilters();
+		        logger.info("Price is selected from Low to High range");
+		        
+	 
+		        String gameName = activities.getCheapestGame();
+		        String gameTime = activities.getCheapestGameTime();
+	 
+		        System.out.println("Cheapest Activity This Weekend: " + gameName);
+		        System.out.println("Date/Time: " + gameTime);
+		        Assert.assertTrue(gameName != null && !gameName.isEmpty(), "Game name should be displayed");
+		        logger.info("Test passed");
+	    	}
+	    	
+	    	catch(Exception e)
+	    	{
+	    		logger.error("Test failed: "+e.getMessage());
+	    		Assert.fail("Test failed: "+e.getMessage());
+	    	}
+	    	
+	    	finally {
+	    		logger.info("-----Finished TC001_SportsActivities-----");
+	    	}
 	    }
 	}
-
-
-
